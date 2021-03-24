edbd4798ebf571be9155b8dd0307c4aaa89c87b4
Percy Jackson <76390871+PercyAres@users.noreply.github.com>
2021-03-24 19:31:23 +0530

    Create telethon.py

---
+++ telethon.py
@@ -0,0 +1,10 @@
+﻿## This file is made by:-
+## Telegram username - @PercyJackson_Op 
+## Kangers kindly keep credits while kanging or else ur gey
+
+from telethon import TelegramClient
+# Use your own values from my.telegram.org
+api_id = '0000'
+api_hash = 'enter hash here'
+with TelegramClient('anon', api_id, api_hash) as client:
+client.loop.run_until_complete(client.send_message('yo', 'kangers, fuck off!'))
